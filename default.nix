5594a41f5c9bb57ae59b1b2b65d51e36b43ee6ae
Joseph Lucas <lucas.jojo@gmail.com>
2021-08-16 11:43:52 +0200

    Add license and homepage

--- default.nix
+++ default.nix
@@ -9,6 +9,12 @@ pkgs.python3Packages.buildPythonPackage {
   propagatedBuildInputs = [ pkgs.libmilter ];
   doCheck = false; # to avoid the missing `makemap` executable of Sendmail
   pythonImportsCheck = [ "Milter" ];
-  meta.description = ''A python extension module to enable python scripts to attach to Sendmail's libmilter API, enabling filtering of messages as they arrive. Since it's a script, you can do anything you want to the message - screen out viruses, collect statistics, add or modify headers, etc. You can, at any point, tell Sendmail to reject, discard, or accept the message.
-ditional python modules provide for navigating and modifying MIME parts, and sending DSNs or doing CBVs.'';
+  meta = {
+    homepage = https://github.com/sdgathman/pymilter;
+    description = ''
+      A python extension module to enable python scripts to attach to Sendmail's libmilter API, enabling filtering of messages as they arrive. Since it's a script, you can do anything you want to the message - screen out viruses, collect statistics, add or modify headers, etc. You can, at any point, tell Sendmail to reject, discard, or accept the message.
+
+      Additional python modules provide for navigating and modifying MIME parts, and sending DSNs or doing CBVs.'';
+    license = [ pkgs.lib.licenses.gpl2Only ];
+  };
 }
