8df5d32bd580c18aa6f909ffa1d9357cc9fccc42
Joseph Lucas <lucas.jojo@gmail.com>
2021-08-23 19:08:22 +0200

    explicit the checkPhase

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ pkgs.python3Packages.buildPythonPackage {
   pname = "pymilter";
   version = "1.0.5";
   src = if inShell then null else ./.;
-  propagatedBuildInputs = [ 
+  propagatedBuildInputs = [
     pkgs.libmilter
     # A dependency of Milter/dns.py
     pkgs.python3Packages.pydns
@@ -22,16 +22,22 @@ pkgs.python3Packages.buildPythonPackage {
   '';
   nativeBuildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.unzip pkgs.patchelf pkgs.zip];
   postInstall = pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
-    find $out -name "*.so" -exec install_name_tool -change libmilter.dylib ${pkgs.libmilter}/lib/libmilter.dylib {} \;
+    find $out -name "*.so" -exec install_name_tool -change libmilter.dylib ${pkgs.libmilter}/lib/libmilter.dylib {};
   '';
-  checkInputs = [  pkgs.python3Packages.bsddb3 ];
-  
+  checkInputs = [ pkgs.python3Packages.bsddb3 ];
   # CheckPhase needs an access to a dummy /etc/resolv.conf
   #   https://nixos.wiki/wiki/Packaging/Quirks_and_Caveats
   preCheck = pkgs.lib.optionalString pkgs.stdenv.isLinux ''
     echo "nameserver 127.0.0.1" > resolv.conf
     export NIX_REDIRECTS=/etc/resolv.conf=$(realpath resolv.conf) \
     LD_PRELOAD=${pkgs.libredirect}/lib/libredirect.so
+  '';
+  # FIXME: use tox during the checkPhase
+  #   https://github.com/pypa/setuptools/issues/1684
+  checkPhase = ''
+    runHook preCheck
+    python setup.py test
+    runHook postCheck
   '';
   postCheck = "unset NIX_REDIRECTS LD_PRELOAD";
   doCheck = true;
