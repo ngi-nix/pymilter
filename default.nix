a5df9b7366d431d66af61c1876b4bfa2f3e4bb54
Joseph Lucas <lucas.jojo@gmail.com>
2021-08-23 18:20:38 +0200

    doCheck = true and silence as few test as possible

--- default.nix
+++ default.nix
@@ -6,14 +6,35 @@ pkgs.python3Packages.buildPythonPackage {
   pname = "pymilter";
   version = "1.0.5";
   src = if inShell then null else ./.;
-  propagatedBuildInputs = [ pkgs.libmilter ];
-  
+  propagatedBuildInputs = [ 
+    pkgs.libmilter
+    # A dependency of Milter/dns.py
+    pkgs.python3Packages.pydns
+  ];
+  postPatch = ''
+    # NB: all other files have a try: import thread; except: import _thread
+    substituteInPlace Milter/greylist.py --replace 'import thread' 'import _thread'
+
+    # Disable testpolicy.py cause it needs `makemap` to create a fixture file "test/access.db"
+    #   while `makemap` is not available yet. See https://github.com/ngi-nix/ngi/issues/91
+    rm testpolicy.py
+    substituteInPlace test.py --replace 'import testpolicy' \'\'
+  '';
   nativeBuildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.unzip pkgs.patchelf pkgs.zip];
   postInstall = pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
     find $out -name "*.so" -exec install_name_tool -change libmilter.dylib ${pkgs.libmilter}/lib/libmilter.dylib {} \;
   '';
-
-  doCheck = false;  # to avoid the missing `makemap` executable of Sendmail
+  checkInputs = [  pkgs.python3Packages.bsddb3 ];
+  
+  # CheckPhase needs an access to a dummy /etc/resolv.conf
+  #   https://nixos.wiki/wiki/Packaging/Quirks_and_Caveats
+  preCheck = pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+    echo "nameserver 127.0.0.1" > resolv.conf
+    export NIX_REDIRECTS=/etc/resolv.conf=$(realpath resolv.conf) \
+    LD_PRELOAD=${pkgs.libredirect}/lib/libredirect.so
+  '';
+  postCheck = "unset NIX_REDIRECTS LD_PRELOAD";
+  doCheck = true;
   pythonImportsCheck = [ "Milter" ];
   meta = {
     homepage = https://github.com/sdgathman/pymilter;
